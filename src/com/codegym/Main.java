14de3eebf113a648f102e2b424aba93998db7d9d
phuquyit@gmail.com <Ruac0n@t2bn>
2020-04-20 21:19:13 +0700

    [Bài tập] Triển khai Facade Pattern

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RandomListFacade randomListFacade = new RandomListFacade(50,5,100);
+        randomListFacade.printRandomEvenList();
+    }
+}
